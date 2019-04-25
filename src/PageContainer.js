3a1723e664c07df20ad7b8ef14510a6b24307514
Dragan Mijatovic <gandra404@yahoo.com>
2019-04-25 17:44:41 +0200

    Sentry

--- src/PageContainer.js
+++ src/PageContainer.js
@@ -3,8 +3,19 @@ import { connect } from 'react-redux';
 import PublicRoutes from './pages/public/PublicPagesContainer';
 import ProtectedRoutes from './pages/protected/ProtectedPagesContainer';
 import Loading from './components/notifications/Loading';
+import * as Sentry from '@sentry/browser';
 
 class PageContainer extends Component {
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ error });
+        Sentry.withScope(scope => {
+            scope.setExtras(errorInfo);
+            const eventId = Sentry.captureException(error);
+            this.setState({eventId})
+        });
+    }
+
     render() {
         return (
             <>
