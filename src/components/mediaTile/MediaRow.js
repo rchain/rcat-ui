9a5c13c9de7cd6d2e50455c2dda232393521ae7e
Ivana Arizanovic <ivana.arizanovic@hotmail.com>
2019-09-04 09:34:26 +0200

    UPDATE: img song placeholder

--- src/components/mediaTile/MediaRow.js
+++ src/components/mediaTile/MediaRow.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 
 class MediaRow extends Component {
-	render() {
-		const img = this.props.img;
-		const imgSrc = 'https://storage.cloud.google.com/r_song/' + img;
-
-		return (
-			<div className="w-100 h4 white bg-black mb2 flex">
-				<img className="w-160" src={imgSrc} alt="album_img"/>
-				<div className="pa3">
-					<span className="f3 dib pb2">{this.props.name}</span><br/>
-					<span className="yellow f5 dib pt1">{this.props.status}</span>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        const img = this.props.img;
+        const imgSrc = 'https://storage.cloud.google.com/r_song/' + img;
+        const imgPlaceholder = <div className="w-160 bg-light-yellow"/>;
+        const imgTab = img == null ? imgPlaceholder : <img className="w-160" src={imgSrc} alt="album_img"/>
+        return (
+            <div className="w-100 h4 white bg-black mb2 flex">
+                {imgTab}
+                <div className="pa3">
+                    <span className="f3 dib pb2">{this.props.name}</span><br/>
+                    <span className="yellow f5 dib pt1">{this.props.status}</span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default MediaRow;
