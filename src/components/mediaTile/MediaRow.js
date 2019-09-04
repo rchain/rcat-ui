9ff6d316544fae8f2da502f8ec9879853dca3aa2
Ivana Arizanovic <ivana.arizanovic@hotmail.com>
2019-09-04 12:36:49 +0200

    UPDATE: img song placeholder path added

--- src/components/mediaTile/MediaRow.js
+++ src/components/mediaTile/MediaRow.js
@@ -3,9 +3,25 @@ import React, {Component} from 'react';
 class MediaRow extends Component {
     render() {
         const img = this.props.img;
-        const imgSrc = 'https://storage.cloud.google.com/r_song/' + img;
+        const imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + img;
         const imgPlaceholder = <div className="w-160 bg-light-yellow"/>;
-        const imgTab = img == null ? imgPlaceholder : <img className="w-160" src={imgSrc} alt="album_img"/>
+        let imgTab = <img className="w-160" src={imgSrc} alt="album_img"/>;
+
+        if (process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH === undefined) {
+            console.log('ERROR FROM MediaRow.js => REACT_APP_GOOGLE_CLOUD_IMAGE_PATH is undefined.');
+            imgTab = imgPlaceholder
+        }
+        else {
+            if ( process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH.length === 0) {
+                console.log('ERROR FROM MediaRow.js => REACT_APP_GOOGLE_CLOUD_IMAGE_PATH is empty');
+                imgTab = imgPlaceholder
+            }
+        }
+        if (img == null ) {
+            console.log('ERROR FROM MediaRow.js => this.props.img is missing');
+            imgTab = imgPlaceholder
+        }
+
         return (
             <div className="w-100 h4 white bg-black mb2 flex">
                 {imgTab}
