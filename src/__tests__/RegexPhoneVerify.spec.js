b3096c8e1ead17bc13befc04ea9dbe8963d20631
Ivana Arizanovic <ivana.arizanovic@hotmail.com>
2019-09-17 16:44:21 +0200

    UPDATE: phone regex test file

--- src/__tests__/RegexPhoneVerify.spec.js
+++ src/__tests__/RegexPhoneVerify.spec.js
@@ -3,7 +3,12 @@ import { validatePhone } from '../validators/validatorHelper';
 describe('RegExp: PhoneNumber', function(){
     it('should validate phone number 1', function(){
 
-        const phone = '+381628210400';
+        const phone = '(577) 288-8001';
+        expect(validatePhone(phone)).toBe(true);
+    });
+    it('should validate phone number 2', function(){
+
+        const phone = '+1-202-555-0162';
         expect(validatePhone(phone)).toBe(true);
     });
     it('should validate phone number 3', function(){
@@ -11,14 +16,29 @@ describe('RegExp: PhoneNumber', function(){
         const phone = '+1-202-575-0122';
         expect(validatePhone(phone)).toBe(true);
     });
-    it('should validate phone number 2', function(){
+    it('should validate phone number 4', function(){
 
         const phone = '+1-202-5n5-0122';
         expect(validatePhone(phone)).toBe(false);
     });
-    it('should validate phone number 2', function(){
+    it('should validate phone number 5', function(){
 
         const phone = '+das dsa 772';
         expect(validatePhone(phone)).toBe(false);
+    });
+    it('should validate phone number 6', function(){
+
+        const phone = '202-555-0172';
+        expect(validatePhone(phone)).toBe(true);
+    });
+    it('should validate phone number 7', function(){
+
+        const phone = '(497) 658-3018';
+        expect(validatePhone(phone)).toBe(true);
+    });
+    it('should validate phone number 8', function(){
+
+        const phone = '+1-202-555-0191';
+        expect(validatePhone(phone)).toBe(true);
     });
 });
