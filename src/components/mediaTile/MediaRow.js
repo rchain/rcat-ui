35595131d57429dd2b51a1debd365f8e528c0601
Ivana Arizanovic <ivana.arizanovic@hotmail.com>
2019-09-18 15:28:38 +0200

    UPDATE: image promise

--- src/components/mediaTile/MediaRow.js
+++ src/components/mediaTile/MediaRow.js
@@ -5,43 +5,36 @@ class MediaRow extends Component {
         super(props)
         this.state = {
             imgTab: <div className="w-160 bg-light-yellow"/>,
-            imagePath: process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH,
-            img: this.props.img,
-            hello:'dassss'
         };
     }
 
-     loadImage = (src) => {
-        return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.addEventListener("load", e => resolve(img));
-            img.addEventListener("error", new Error("MediaRow => Error with img"));
-            img.src = src;
-        })
-    };
 
-
-    componentWillMount() {
-        console.log('hello from component')
-
-        let imgSrc = this.imagePath + this.img;
-
-        this.loadImage(imgSrc)
-            .then(img => {
-                this.setState({
-                    imgTag: img,
-                    hello: 'hellohellohello'
-                })
+    componentDidMount() {
+        let imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + this.props.img;
+        const loadImage = (src) => {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.addEventListener("load", img => resolve(img));
+                img.addEventListener('error', () => {
+                    reject(new Error(`Failed to load image`));
+                });
+                img.src = src;
+                console.log(img)
             })
-            .catch(error => console.log(error));
-
+        };
+        loadImage(imgSrc).then(img => {
+            console.log(img)
+            let imageSrc = <img className="w-160" src={img.src}/>
+            this.setState({
+                imgTab: imageSrc,
+            })
+        }).catch(error => console.log(error));
     }
 
     render() {
         return (
             <div className="w-100 h4 white bg-black mb2 flex">
                 {this.state.imgTab}
-                <p>{this.state.hello}</p>
                 <div className="pa3">
                     <span className="f3 dib pb2">{this.props.name}</span><br/>
                     <span className="yellow f5 dib pt1">{this.props.status}</span>
