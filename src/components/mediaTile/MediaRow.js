00cfb9adf6a03d27a918b94714bb9eabb74d614f
Ivana Arizanovic <ivana.arizanovic@hotmail.com>
2019-09-18 12:21:31 +0200

    UPDATE: img promise

--- src/components/mediaTile/MediaRow.js
+++ src/components/mediaTile/MediaRow.js
@@ -2,20 +2,43 @@ import React, {Component} from 'react'
 
 class MediaRow extends Component {
     render() {
-        const img = this.props.img;
-        // const imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + img;
-        //imgTab = <img className="w-160" src={img.src} alt="album_img"/>
+        // const img = this.props.img;
+        // // const imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + img;
+        // //imgTab = <img className="w-160" src={img.src} alt="album_img"/>
+         let imgTab =  <div className="w-160 bg-light-yellow"/>;
+        // let imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + img;
 
-        let imgTab = <div className="w-160 bg-light-yellow"/>
-        let imgSrc;
-        const loadImage = (img) =>{
-            return new Promise((resolve, reject)=> {
-                resolve(imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + img);
-                reject(error => {console.log(error)})
-            })};
+        let imgSrc = this.imagePath + this.img;
+        // let imgSrc;
+        // const loadImage = (img) => {
+        //     return new Promise((resolve, reject) => {
+        //             resolve(
+        //                 imgSrc = process.env.REACT_APP_GOOGLE_CLOUD_IMAGE_PATH + img,
+        //                 imgTab = <img className="w-160" src={imgSrc} alt="album_img"/>,
+        //                 console.log(imgSrc)
+        //             );
+        //         reject(
+        //             error => console.log('reject error',error)
+        //         )
+        //     }).then().catch(error => console.log('catch error',error))
+        // };
+        //
+        // loadImage(img);
 
-        loadImage(img);
-        imgTab = <img className="w-160" src={imgSrc} />
+        function loadImage(src) {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.addEventListener("load", e => resolve(img));
+                img.addEventListener("error", new Error("MediaRow => Error with img"));
+                img.src = src;
+            })
+        };
+
+        loadImage(imgSrc)
+            .then(img => imgTab = <img className="w-160" src={img.src} alt="album_img"/>)
+            .catch(error => console.log(error));
+
+
         return (
             <div className="w-100 h4 white bg-black mb2 flex">
                 {imgTab}
